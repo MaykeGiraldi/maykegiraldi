a1f77b183931b3bf95673f29fab81905fc2180e3
Mayke Giraldi <maykegirralde@gmail.com>
2026-01-08 15:29:24 -0300

    Resolve merge conflict

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const cursorLight = document.getElementById("cursor-light");
 
 document.addEventListener("mousemove", (e) => {
@@ -13,12 +12,3 @@ botaoContato.addEventListener('click', function (event) {
     event.preventDefault()
     secaoContato.scrollIntoView({ behavior: 'smooth'})
 })
-=======
-const cursorLight = document.getElementById("cursor-light");
-
-document.addEventListener("mousemove", (e) => {
-    cursorLight.style.left = `${e.clientX}px`;
-    cursorLight.style.top = `${e.clientY}px`;
-});
-
->>>>>>> 7588e56e50bfa7b2f491a65f7cfa7e46fe43e1b6
